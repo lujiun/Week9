11043d2459a069ad5dc1595a904b4a90b828898c
Taejins <ktj0419@naver.com>
2021-06-09 20:18:21 +0900

    NewSchedule 지도 구현

--- app/src/main/java/com/example/week9/NewSchedule.java
+++ app/src/main/java/com/example/week9/NewSchedule.java
@@ -1,14 +1,98 @@
 package com.example.week9;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class NewSchedule extends AppCompatActivity {
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+public class NewSchedule extends AppCompatActivity implements OnMapReadyCallback {
+    private FusedLocationProviderClient mFusedLocationClient;
+    GoogleMap mGoogleMap = null;
+    private Geocoder geocoder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_schedule);
+
+        //지도 이용
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        Button map_btn = findViewById(R.id.ns_bt1);
+        map_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                 getLocation();
+            }
+        });
+    }
+    private void getLocation() {
+        EditText map_text = findViewById(R.id.ns_ed2);
+        String search_address = map_text.getText().toString();
+        try {
+            List<Address> addresses = geocoder.getFromLocationName(search_address,5);
+            if (addresses.size() >0) {
+                Address bestResult = (Address) addresses.get(0);
+                LatLng location = new LatLng(bestResult.getLatitude(),bestResult.getLongitude());
+                Toast.makeText(getApplicationContext(),
+                        "No"+bestResult.getLatitude(),
+                        Toast.LENGTH_SHORT)
+                        .show();
+                mGoogleMap.addMarker(new MarkerOptions().
+                        position(location).
+                        title("검색 위치").
+                        alpha(0.8f));
+                mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location,15));
+            }
+        } catch (IOException e) {
+            Log.e(getClass().toString(),"Failed in using Geocoder.", e);
+            return;
+        }
+    }
+
+    public void onMapReady(GoogleMap googleMap) {
+        mGoogleMap = googleMap;
+        geocoder = new Geocoder(this, Locale.KOREA);
+        //여기에 선택된 인자 받아서 처음 들어왔을 때 저장했던 위치 뜨게 할 거임.
+
+        // move the camera
+        mGoogleMap.setOnMarkerClickListener(new MyMarkerClickListener());
+    }
+    class MyMarkerClickListener implements GoogleMap.OnMarkerClickListener {
+        @Override
+        public boolean onMarkerClick(Marker marker) {
+            mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(marker.getPosition(),15));
+            return false;
+        }
     }
 }
